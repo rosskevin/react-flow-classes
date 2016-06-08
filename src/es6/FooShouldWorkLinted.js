a20bd0b8badf15d3f2850250a0e94fe189f58754
Kevin Ross <kevin.ross@alienfast.com>
2016-06-08 17:29:49 -0500

    bringing in classes samples from flow (Foo)

---
+++ src/es6/FooShouldWorkLinted.js
@@ -0,0 +1,44 @@
+// @flow
+import React from 'react'
+
+// Taken from https://github.com/facebook/flow/blob/master/tests/new_react/classes.js
+// - render method added
+// - convert errors to make it pass...maybe?
+
+type DefaultProps = { }
+type Props = { x: number }
+type State = { y: number }
+
+class Foo extends React.Component {
+  props:Props
+  state:State
+  static defaultProps:DefaultProps
+
+  is_mounted:boolean
+
+  static bar ():void {}
+
+  qux ():void {
+    const xx :number = this.props.x
+  }
+
+  constructor (props) {
+    super(props)
+    this.setState({ y: 2 })
+  }
+
+  setState (o:{ y: number }):void { }
+
+  componentDidMount ():void {
+    this.is_mounted = true
+  }
+
+  componentWillReceiveProps (nextProps:Object,
+                             nextContext:any):void {
+    this.qux()
+  }
+
+  render () {
+    return <div>Hello World</div>
+  }
+}
