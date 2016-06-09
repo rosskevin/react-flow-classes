fc98b984254ae494e537ab60998208c8b8066448
Kevin Ross <kevin.ross@alienfast.com>
2016-06-09 10:25:02 -0500

    .

--- src/es6/Works.js
+++ src/es6/Works.js
@@ -1,10 +1,6 @@
 // @flow
 import React from 'react'
 
-// Taken from https://github.com/facebook/flow/blob/master/tests/new_react/classes.js
-// - render method added
-// - convert errors to make it pass
-
 type DefaultProps = { x: 1 }
 type Props = { x: number }
 type State = { y: number }
