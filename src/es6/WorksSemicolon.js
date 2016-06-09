3c56fd9b7efe4bbcf4ca434e57af0b8c9b5218b7
Kevin Ross <kevin.ross@alienfast.com>
2016-06-09 10:26:44 -0500

    single semicolon example makes it work

---
+++ src/es6/WorksSemicolon.js
@@ -0,0 +1,23 @@
+// @flow
+import React from 'react'
+
+type DefaultProps = { x: 1 }
+type Props = { x: number }
+type State = { y: number }
+
+class Foo extends React.Component {
+  props:Props
+  state:State
+  static defaultProps:DefaultProps;
+
+  // fails without this static method above constructor
+  //static bar ():void {}
+
+  constructor (props) {
+    super(props)
+  }
+
+  render () {
+    return <div>Hello World</div>
+  }
+}
